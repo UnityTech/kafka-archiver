ec2a964268949861e475f85a94bbbbe878e21fd8
Vincenzo Prignano <vincenzo.prignano@gmail.com>
2017-10-06 11:45:51 -0700

    Simplify tests in uploader_test.go

--- uploader_test.go
+++ uploader_test.go
@@ -15,13 +15,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUploadWithDefaultPartitioner(t *testing.T) {
+func newTestWorker(partitioner partitioner.Partitioner, s3Client *s3.S3) *Worker {
 	w := NewWorker()
 	w.s3enabled = true
 	w.s3Concurrency = 1
-	w.partitioner = partitioner.New(&partitioner.Config{Type: "DefaultPartitioner"})
-	w.s3bucket = "test-s3-bucket"
+	w.partitioner = partitioner
+	w.s3bucket = "test"
+	w.uploader = s3manager.NewUploaderWithClient(s3Client)
+	return w
+}
 
+func TestUploadWithDefaultPartitioner(t *testing.T) {
 	s3ClientMock := s3.New(session.New())
 	s3ClientMock.Handlers.Clear()
 	s3ClientMock.Handlers.Send.PushBack(func(r *request.Request) {
@@ -31,21 +35,20 @@ func TestUploadWithDefaultPartitioner(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, "{\"message\": 1}{\"message\": 2}{\"message\": 3}", string(bt))
 			require.Contains(t, *x.Key, "/testTopic1/partition=10/0010.5555.")
-			require.Equal(t, *x.Bucket, w.s3bucket)
+			require.Equal(t, *x.Bucket, "test")
 		}
 	})
 
-	w.uploader = s3manager.NewUploaderWithClient(s3ClientMock)
-
+	// Create new temporary file to upload.
 	f, err := ioutil.TempFile("", "kafka-archiver-upload-test")
 	require.NoError(t, err)
-
 	f.WriteString(`{"message": 1}`)
 	f.WriteString(`{"message": 2}`)
 	f.WriteString(`{"message": 3}`)
-
 	require.NoError(t, f.Close())
 
+	// Create worker and upload.
+	w := newTestWorker(partitioner.New(&partitioner.Config{Type: "DefaultPartitioner"}), s3ClientMock)
 	w.upload(&buffer.Flush{
 		Topic:      "testTopic1",
 		Path:       f.Name(),
@@ -55,17 +58,6 @@ func TestUploadWithDefaultPartitioner(t *testing.T) {
 }
 
 func TestUploadWithTimeFieldPartitioner(t *testing.T) {
-	w := NewWorker()
-	w.s3enabled = true
-	w.s3Concurrency = 1
-	w.partitioner = partitioner.New(&partitioner.Config{
-		Type:            "TimeFieldPartitioner",
-		FieldKeyName:    "ts",
-		TopicNamePrefix: "topic=",
-		BaseFolder:      "backup",
-	})
-	w.s3bucket = "test-s3-bucket-timefield"
-
 	s3ClientMock := s3.New(session.New())
 	s3ClientMock.Handlers.Clear()
 	s3ClientMock.Handlers.Send.PushBack(func(r *request.Request) {
@@ -75,20 +67,24 @@ func TestUploadWithTimeFieldPartitioner(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, "{\"message\": 1, \"ts\": 1444262400}{\"message\": 2, \"ts\": 1444262401}{\"message\": 3, \"ts\": 1444262402}", string(bt))
 			require.Contains(t, *x.Key, "backup/topic=testTopic2/year=2015/month=10/day=08/hour=00/0010.808.")
-			require.Equal(t, *x.Bucket, w.s3bucket)
+			require.Equal(t, *x.Bucket, "test")
 		}
 	})
 
-	w.uploader = s3manager.NewUploaderWithClient(s3ClientMock)
-
+	// Create new temporary file to upload.
 	f, err := ioutil.TempFile("", "kafka-archiver-upload-test")
 	require.NoError(t, err)
-
 	f.WriteString(`{"message": 1, "ts": 1444262400}`)
 	f.WriteString(`{"message": 2, "ts": 1444262401}`)
 	f.WriteString(`{"message": 3, "ts": 1444262402}`)
-
 	require.NoError(t, f.Close())
+
+	w := newTestWorker(partitioner.New(&partitioner.Config{
+		Type:            "TimeFieldPartitioner",
+		FieldKeyName:    "ts",
+		TopicNamePrefix: "topic=",
+		BaseFolder:      "backup",
+	}), s3ClientMock)
 
 	w.upload(&buffer.Flush{
 		Topic:      "testTopic2",
@@ -100,17 +96,6 @@ func TestUploadWithTimeFieldPartitioner(t *testing.T) {
 }
 
 func TestUploadWithIsoDateFieldPartitioner(t *testing.T) {
-	w := NewWorker()
-	w.s3enabled = true
-	w.s3Concurrency = 1
-	w.partitioner = partitioner.New(&partitioner.Config{
-		Type:            "IsoDateFieldPartitioner",
-		FieldKeyName:    "ts",
-		TopicNamePrefix: "topic=",
-		BaseFolder:      "backup",
-	})
-	w.s3bucket = "test-s3-bucket-timefield"
-
 	s3ClientMock := s3.New(session.New())
 	s3ClientMock.Handlers.Clear()
 	s3ClientMock.Handlers.Send.PushBack(func(r *request.Request) {
@@ -120,15 +105,13 @@ func TestUploadWithIsoDateFieldPartitioner(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, "{\"message\": 1, \"ts\": \"2015-10-08T00:00:00Z\"}{\"message\": 2, \"ts\": \"2015-10-08T00:00:01Z\"}{\"message\": 3, \"ts\": \"2015-10-08T00:00:02Z\"}", string(bt))
 			require.Contains(t, *x.Key, "backup/topic=testTopic2/date=2015-10-08/hour=00/0010.808.")
-			require.Equal(t, *x.Bucket, w.s3bucket)
+			require.Equal(t, *x.Bucket, "test")
 		}
 	})
 
-	w.uploader = s3manager.NewUploaderWithClient(s3ClientMock)
-
+	// Create new temporary file to upload.
 	f, err := ioutil.TempFile("", "kafka-archiver-upload-test")
 	require.NoError(t, err)
-
 	f.WriteString(`{"message": 1, "ts": "2015-10-08T00:00:00Z"}`)
 	f.WriteString(`{"message": 2, "ts": "2015-10-08T00:00:01Z"}`)
 	f.WriteString(`{"message": 3, "ts": "2015-10-08T00:00:02Z"}`)
@@ -137,6 +120,13 @@ func TestUploadWithIsoDateFieldPartitioner(t *testing.T) {
 
 	hourBucket, err := time.Parse(time.RFC3339Nano, "2015-10-08T00:00:00Z")
 	require.NoError(t, err)
+
+	w := newTestWorker(partitioner.New(&partitioner.Config{
+		Type:            "IsoDateFieldPartitioner",
+		FieldKeyName:    "ts",
+		TopicNamePrefix: "topic=",
+		BaseFolder:      "backup",
+	}), s3ClientMock)
 
 	w.upload(&buffer.Flush{
 		Topic:      "testTopic2",
